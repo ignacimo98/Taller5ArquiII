b07ec86d173c62021d4e2da6f1fdea0d91d1c4d6
Ignacio Mora <ignacimo98@hotmail.com>
2019-11-20 22:15:21 -0600

    Changed sizes and threads

--- helloworld/vecadd.cu
+++ helloworld/vecadd.cu
@@ -26,16 +26,15 @@ void vecAdd_h(int *A1, int *B1, int *C1, int N) {
 }
 
 int main(int argc, char **argv) {
-  printf("Begin \n");
   // Vector
-  int n = 100000000;
+  int n = 1 << 25;
   // Block size and number
   int block_size, block_no;
   
 
-  for (int j = 0; j < 3; ++j) {
-    block_size = 250;  // threads per block
-    for (int k = 0; k < 2; ++k) {
+  for (int j = 0; j < 8; ++j) {
+    block_size = 256;  // threads per block
+    for (int k = 0; k < 4; ++k) {
       // Number of blocks
       int nBytes = n * sizeof(int);
 
@@ -56,18 +55,17 @@ int main(int argc, char **argv) {
       // Data filling
       for (int i = 0; i < n; i++) a[i] = i, b[i] = i;
 
-      printf("Allocating device memory on host..\n");
       // GPU memory allocation
       cudaMalloc((void **)&a_d, n * sizeof(int));
       cudaMalloc((void **)&b_d, n * sizeof(int));
       cudaMalloc((void **)&c_d, n * sizeof(int));
 
-      printf("Copying to device..\n");
+
       cudaMemcpy(a_d, a, n * sizeof(int), cudaMemcpyHostToDevice);
       cudaMemcpy(b_d, b, n * sizeof(int), cudaMemcpyHostToDevice);
 
       clock_t start_d = clock();
-      printf("Doing GPU Vector add\n");
+
       vecAdd<<<block_no, block_size>>>(a_d, b_d, c_d, n);
       cudaCheckError();
 
@@ -76,7 +74,7 @@ int main(int argc, char **argv) {
 
       clock_t end_d = clock();
 
-      printf("Doing CPU Vector add\n");
+
       clock_t start_h = clock();
       vecAdd_h(a, b, c2, n);
       clock_t end_h = clock();
@@ -88,16 +86,16 @@ int main(int argc, char **argv) {
       // Copying data back to host, this is a blocking call and will not start
       // until all kernels are finished
       cudaMemcpy(c, c_d, n * sizeof(int), cudaMemcpyDeviceToHost);
-      printf("n = %d \tblockSize = %d \tGPU time = %fs \tCPU time = %fs\n", n,
-             block_size, time_d, time_h);
+      printf("n = %d \tblockSize = %d \tGPU time = %fs \tCPU time = %fs \tSpeedup =%f\n", n,
+             block_size, time_d, time_h, time_h/time_d);
 
       // Free GPU memory
       cudaFree(a_d);
       cudaFree(b_d);
       cudaFree(c_d);
-      block_size /= 5;
+      block_size >>1;
     }
-    n/=10;
+    n>>=1;
   }
   return 0;
 }
